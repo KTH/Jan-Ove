8e56d1b7d3a0e03a7b3cc7a69f4d32da96882d56
Jens Tinglev <tinglev@kth.se>
2019-04-15 14:57:44 +0200

    Added tests

--- test/test_util.py
+++ test/test_util.py
@@ -5,19 +5,31 @@ from modules import util
 
 class TestUtil(unittest.TestCase):
 
-    def test_args_to_commands(self):
+    def test_3_words_normal_quotes(self):
         test_data = 'new-season "name of season"'
         result = util.args_to_commands(test_data)
         self.assertEqual(len(result), 2)
         self.assertEqual(result, ['new-season', 'name of season'])
+
+    def test_curly_quotes(self):
+        test_data = 'new-season “name of season”'
+        result = util.args_to_commands(test_data)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result, ['new-season', 'name of season'])
+
+    def test_1_word_normal_quotes(self):
         test_data = 'new-season "season"'
         result = util.args_to_commands(test_data)
         self.assertEqual(len(result), 2)
         self.assertEqual(result, ['new-season', 'season'])
+
+    def test_2_words_normal_quotes(self):
         test_data = 'new-season "season 1"'
         result = util.args_to_commands(test_data)
         self.assertEqual(len(result), 2)
         self.assertEqual(result, ['new-season', 'season 1'])
+
+    def test_multiple_args_no_quotes(self):
         test_data = 'register-result @tinglev @hoyce 10 0'
         result = util.args_to_commands(test_data)
         self.assertEqual(len(result), 5)
